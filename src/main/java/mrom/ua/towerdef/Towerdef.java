7740b91ab5af4f2eefa5fdd23ec8ef069f3636ab
BuildTools <unconfigured@null.spigotmc.org>
2023-08-27 17:46:04 +0300

    Created

---
+++ src/main/java/mrom/ua/towerdef/Towerdef.java
@@ -0,0 +1,17 @@
+package mrom.ua.towerdef;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Towerdef extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
